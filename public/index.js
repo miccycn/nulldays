d6796e044e31428ceff64f64d996cfb1d167f8e7
韦元悦 <yuanyue@staff.weibo.com>
2016-06-13 18:10:51 +0800

    增加发布文字的边界条件提示语，修改样式

---
+++ public/index.js
@@ -0,0 +1,51 @@
+( function() {
+var cardList = new Vue({
+    el: "#board",
+    data: {
+        lists: []
+    },
+    filters: {
+        timeFormat: function(time) {
+            var timing = new Date(time);
+            return timing.getFullYear() + "年" + (timing.getMonth() + 1) + "月" + timing.getDate() + "日 " + timing.toLocaleTimeString();
+        }
+    }
+})
+
+var form = document.getElementById("form");
+var input = document.getElementById("box");
+var inputTextBefore = "";
+
+input.onkeydown = function(event) {
+    if (event.keyCode == 13) {
+        var inputText = input.value.trim();
+        if (inputText.length <= 0) {
+            return false;
+        }
+        if (inputText == inputTextBefore) {
+            alert("你已经说过了噢")
+            input.value = "";
+            return false;
+        }
+        if (inputText.length > 300) {
+            alert("你说的话有点多了哈哈哈哈哈")
+            return false;
+        }
+        if (inputText.length > 0 && inputText.length < 300) {
+            inputTextBefore = inputText;
+            this.form.submit();
+            input.value = "";
+        }
+    }
+};
+
+var socket = io.connect(':4000');
+
+socket.on("data", function(item) {
+    cardList.lists.unshift(item);
+});
+
+socket.on("new", function(lists) {
+    cardList.lists = lists;
+});
+} )()
